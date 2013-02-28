a9fcea8dfb3b7a7bba5b6df52e5a88ee63f49d03
Blake Thomson <thomsbg@gmail.com>
2013-02-27 19:49:52 -0500

    Update nginx version

--- nginx.rb
+++ nginx.rb
@@ -3,31 +3,33 @@ require 'formula'
 class NginxPushStreamModule < Formula
   homepage 'http://github.com/wandenberg/nginx-push-stream-module'
   url 'git://github.com/wandenberg/nginx-push-stream-module.git', :using => :git
-  def initialize; super "NginxPushStreamModule"; end
+  def initialize; super 'NginxPushStreamModule'; end
 end
 
 
 class NginxHttpAuthRequestModule < Formula
   homepage 'http://mdounin.ru/hg/ngx_http_auth_request_module'
   url 'http://mdounin.ru/hg/ngx_http_auth_request_module', :using => :hg
-  def initialize; super "NginxHttpAuthRequestModule"; end
+  def initialize; super 'NginxHttpAuthRequestModule'; end
 end
-
 
 class Nginx < Formula
   homepage 'http://nginx.org/'
-  url 'http://nginx.org/download/nginx-1.2.3.tar.gz'
-  sha1 '98059ae08ebbfaaead868128f7b66ebce16be9af'
+  url 'http://nginx.org/download/nginx-1.2.7.tar.gz'
+  sha1 '65309abde9d683ece737da7a354c8fae24e15ecb'
 
   devel do
-    url 'http://nginx.org/download/nginx-1.3.5.tar.gz'
-    sha1 'ce0245295f23a54f10d916eb6b7b34469d0618a1'
+    url 'http://nginx.org/download/nginx-1.3.13.tar.gz'
+    sha1 'b09b1c35b2b741292d41db1caa3b8a4123805a4c'
   end
+
+  env :userpaths
 
   depends_on 'pcre'
 
   option 'with-passenger', 'Compile with support for Phusion Passenger module'
   option 'with-webdav', 'Compile with support for WebDAV module'
+  option 'with-debug', 'Compile with support for debug log'
   option 'with-push-stream', 'Compile with support for Push Stream module'
   option 'with-http-auth-request', 'Compile with support for HTTP Auth Request module'
 
@@ -39,26 +41,26 @@ class Nginx < Formula
   end
 
   def passenger_config_args
-      passenger_root = `passenger-config --root`.chomp
+    passenger_root = `passenger-config --root`.chomp
 
-      if File.directory?(passenger_root)
-        return "--add-module=#{passenger_root}/ext/nginx"
-      end
+    if File.directory?(passenger_root)
+      return "--add-module=#{passenger_root}/ext/nginx"
+    end
 
-      puts "Unable to install nginx with passenger support. The passenger"
-      puts "gem must be installed and passenger-config must be in your path"
-      puts "in order to continue."
-      exit
+    puts "Unable to install nginx with passenger support. The passenger"
+    puts "gem must be installed and passenger-config must be in your path"
+    puts "in order to continue."
+    exit
   end
 
-  def push_stream
-      NginxPushStreamModule.new.brew { (buildpath + 'nginx-push-stream-module').install Dir['*'] }
-      "--add-module=nginx-push-stream-module"
+  def push_stream_args
+    NginxPushStreamModule.new.brew { (buildpath + 'nginx-push-stream-module').install Dir['*'] }
+    "--add-module=nginx-push-stream-module"
   end
 
-  def http_auth_request
-      NginxHttpAuthRequestModule.new.brew { (buildpath + 'nginx-http-auth-request-module').install Dir['*'] }
-      "--add-module=nginx-http-auth-request-module"
+  def http_auth_request_args
+    NginxHttpAuthRequestModule.new.brew { (buildpath + 'nginx-http-auth-request-module').install Dir['*'] }
+    "--add-module=nginx-http-auth-request-module"
   end
 
   def install
@@ -70,20 +72,24 @@ class Nginx < Formula
             "--with-ld-opt=-L#{HOMEBREW_PREFIX}/lib",
             "--conf-path=#{etc}/nginx/nginx.conf",
             "--pid-path=#{var}/run/nginx.pid",
-            "--lock-path=#{var}/nginx/nginx.lock"]
+            "--lock-path=#{var}/run/nginx.lock",
+            "--http-client-body-temp-path=#{var}/run/nginx/client_body_temp",
+            "--http-proxy-temp-path=#{var}/run/nginx/proxy_temp",
+            "--http-fastcgi-temp-path=#{var}/run/nginx/fastcgi_temp",
+            "--http-uwsgi-temp-path=#{var}/run/nginx/uwsgi_temp",
+            "--http-scgi-temp-path=#{var}/run/nginx/scgi_temp"]
 
     args << passenger_config_args if build.include? 'with-passenger'
     args << "--with-http_dav_module" if build.include? 'with-webdav'
-    args << push_stream if build.include? 'with-push-stream'
-    args << http_auth_request if build.include? 'with-http-auth-request'
+    args << "--with-debug" if build.include? 'with-debug'
+    args << push_stream_args if build.include? 'with-push-stream'
+    args << http_auth_request_args if build.include? 'with-http-auth-request'
 
     system "./configure", *args
     system "make"
     system "make install"
     man8.install "objs/nginx.8"
-
-    plist_path.write startup_plist
-    plist_path.chmod 0644
+    (var/'run/nginx').mkpath
   end
 
   def caveats; <<-EOS.undent
@@ -104,29 +110,30 @@ class Nginx < Formula
     EOS
   end
 
-  def startup_plist
-    return <<-EOPLIST
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-  <dict>
-    <key>Label</key>
-    <string>#{plist_name}</string>
-    <key>RunAtLoad</key>
-    <true/>
-    <key>KeepAlive</key>
-    <false/>
-    <key>UserName</key>
-    <string>#{`whoami`.chomp}</string>
-    <key>ProgramArguments</key>
-    <array>
-        <string>#{HOMEBREW_PREFIX}/sbin/nginx</string>
-    </array>
-    <key>WorkingDirectory</key>
-    <string>#{HOMEBREW_PREFIX}</string>
-  </dict>
-</plist>
-    EOPLIST
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <false/>
+        <key>UserName</key>
+        <string>#{`whoami`.chomp}</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_prefix}/sbin/nginx</string>
+            <string>-g</string>
+            <string>daemon off;</string>
+        </array>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+      </dict>
+    </plist>
+    EOS
   end
 end
 
